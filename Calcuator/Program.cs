68a21b5d2b7f8046613f7cf267c092a3646c0fcc
Iiro Saloviin <iiisa006@edu.xamk.fi>
2025-09-25 16:21:40 +0300

    Aloitus

---
+++ Calcuator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Calcuator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Choose operation:");
+            Console.WriteLine("1. Add (+)");
+            Console.WriteLine("2. Subtract (-)");
+            Console.WriteLine("3. Multiply (*)");    
+            Console.WriteLine("4. Divide (/)");
+
+            string? choice = Console.ReadLine();
+
+            
+        }
+    }
+}
